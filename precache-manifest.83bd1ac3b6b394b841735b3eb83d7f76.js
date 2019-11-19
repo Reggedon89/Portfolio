af3c9919d245c57386fce18b926736867ae746e0
Geoffrey Hutson <geoff.hutson.89@gmail.com>
2019-11-19 14:42:04 -0800

    Updates

---
+++ precache-manifest.83bd1ac3b6b394b841735b3eb83d7f76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6e31444abe97340f0fed8dd81ff7c9",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a237cc0fccc95f9e5a1e",
+    "url": "/Portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "50b27facfacf66c8e5fe",
+    "url": "/Portfolio/static/js/2.85b64d7f.chunk.js"
+  },
+  {
+    "revision": "a237cc0fccc95f9e5a1e",
+    "url": "/Portfolio/static/js/main.8d83ebc8.chunk.js"
+  },
+  {
+    "revision": "11f789622263dbd65102",
+    "url": "/Portfolio/static/js/runtime-main.b161af14.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
